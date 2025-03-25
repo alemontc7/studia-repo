a72c70e9e3191f77475a9dd7c984cab183403f1e
Alejandro Montero <alejandro.montero@ucb.edu.bo>
2025-03-25 18:23:57 -0400

    Issues fixedd

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,4 +1,4 @@
-import app from '../src/config/server';
+import app from '../config/server';
 //This config is exclusive for the API deployment in vercel
 //yes it is simple but is key to make the API work under the vercel rules
 export default app;
